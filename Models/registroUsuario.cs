02b6e4c50026d146730cc154230ecd813e4ca131
Orion-Ro <ro.asd.adke@gmail.com>
2023-11-10 23:53:30 -0300

    cambios

--- Models/registroUsuario.cs
+++ Models/registroUsuario.cs
@@ -11,15 +11,25 @@ namespace APIformbuilder.Models
         [StringLength(50, MinimumLength = 2, ErrorMessage = "El campo Username debe tener entre 2 y 50 caracteres.")]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "El campo Password es obligatorio.")]
         [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "El campo RoleID es obligatorio.")]
-        public Roles RoleID { get; set; } 
+        //[Required(ErrorMessage = "El campo RoleID es obligatorio.")]
+        public Roles RoleID { get; set; }
+
+        public List<permisos> Permisos { get; set; }
+
+    }
+    public class permisos
+    {
+        public int id { get; set; }
+        public int usuarioId { get; set; }
+        public int permisoId { get; set; }
     }
 
+
 }
+
 
 
 
